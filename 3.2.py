09816664c57997c5541da46fc8df1d0729b8a5e7
qcklll <churikov.it@gmail.com>
2022-01-28 22:51:10 +0300

    Add file

---
+++ 3.2.py
@@ -0,0 +1,6 @@
+names = ["Artem", "Andrei","Ilya","Sergey"]
+mes = ("Privet ")
+print(mes + names[0])
+print(mes + names[1])
+print(mes + names[2])
+print(mes + names[3])
---
+++ 3.8-3.10.py
@@ -0,0 +1,21 @@
+country = ['Russia','Germany','England','Mexico','America']
+print(country)
+print("Sort")
+print(sorted(country))
+print(country)
+
+print("reverse = true")
+print(sorted(country, reverse=True))
+print(country)
+#Переворачиваем его несколько раз
+print("reverse")
+country.reverse()
+print(country)
+country.reverse()
+print(country)
+country.reverse()
+print(country)
+#Сортируем массив
+print("sort")
+country.sort
+print(country)
